96d5a5862dfb878dd4475b0d628f73b56cb16047
Intern Novus <intern@novus.edu.au>
2026-05-07 04:24:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8123516018-556e8",
+  "appId": "1:131197181580:web:7ca5349ccac072d455e5a9",
+  "apiKey": "AIzaSyBUSjOmAvFvQqTnAMajy7AgVlwqRxRWzrY",
+  "authDomain": "studio-8123516018-556e8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "131197181580"
+};
